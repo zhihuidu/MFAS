a4539349989984c7a2ed9c373048928baecea3fe
Zhihui Du <zhihuidu@gmail.com>
2024-09-18 11:24:15 -0400

    update WLS license

--- lp.py
+++ lp.py
@@ -19,11 +19,13 @@ import matplotlib.pyplot as plt
 FileNameHead="lp"
 
 # Set Gurobi license information using environment variables
-os.environ['GRB_WLSACCESSID'] = 'd051c581-fa20-4960-a4fe-4a10026018c5'
-os.environ['GRB_WLSSECRET'] = 'b2f5f047-f697-4b38-987d-44371d5a4e5f'
+os.environ['GRB_WLSACCESSID'] = 'fb436391-3bb5-4b06-9a8c-66f0354b5011'
+os.environ['GRB_WLSSECRET'] = '37c29f28-6ae4-4a19-913d-6b8100964563'
 os.environ['GRB_LICENSEID'] = '2540055'
 
-removed_list=[]
+# Optional: Disable the local license check by unsetting GRB_LICENSE_FILE
+os.environ.pop('GRB_LICENSE_FILE', None)
+
 complete_removed_list=[]
 
 
